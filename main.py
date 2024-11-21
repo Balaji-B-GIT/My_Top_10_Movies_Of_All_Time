763d4dcd5a209d392c12f6d615cc278adabeaf04
Balaji B <balaji.balachandar1@gmail.com>
2024-11-21 13:48:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///top-10-movies.db'
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+
+# CREATE TABLE
+class Movie(db.Model):
+    id:Mapped[int] = mapped_column(Integer,primary_key=True)
+    title:Mapped[str] = mapped_column(String(250),nullable=False,unique=True)
+    year:Mapped[int] = mapped_column(Integer,nullable=False)
+    description:Mapped[str] = mapped_column(String(500),nullable=False)
+    rating:Mapped[float] = mapped_column(Float,nullable=False)
+    ranking:Mapped[int] = mapped_column(Integer,nullable=False)
+    review:Mapped[str] = mapped_column(String,nullable=False)
+    img_url:Mapped[str] = mapped_column(String,nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    all_movies = db.session.execute(db.select(Movie).order_by(Movie.title)).scalars().all()
+    return render_template("index.html",movies = all_movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
