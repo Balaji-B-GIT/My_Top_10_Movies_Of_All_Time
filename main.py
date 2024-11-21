8ea156f868727068a6ced1db0ed09b2bf7c8cfbe
Balaji B <balaji.balachandar1@gmail.com>
2024-11-21 23:50:43 +0530

    Added multiple features

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv("C:/Python/Environmental variables/.env")
+## get these from TMDB----------------------------------
+api_key = os.getenv("tmdb_key")
+api_token = os.getenv("tmdb_token")
+tmdb_url = "https://api.themoviedb.org/3/search/movie"
+tmdb_img_url = "https://image.tmdb.org/t/p/w500"
+##------------------------------------------------------
+headers = {
+    "accept": "application/json",
+    "Authorization": f'Bearer {api_token}'
+}
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -36,6 +49,10 @@ class Update(FlaskForm):
     review = StringField('Your Review', validators=[DataRequired()])
     submit = SubmitField('Done')
 
+class Add(FlaskForm):
+    movie_title = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///top-10-movies.db'
 
 db = SQLAlchemy(model_class=Base)
@@ -49,9 +66,9 @@ class Movie(db.Model):
     title:Mapped[str] = mapped_column(String(250),nullable=False,unique=True)
     year:Mapped[int] = mapped_column(Integer,nullable=False)
     description:Mapped[str] = mapped_column(String(500),nullable=False)
-    rating:Mapped[float] = mapped_column(Float,nullable=False)
-    ranking:Mapped[int] = mapped_column(Integer,nullable=False)
-    review:Mapped[str] = mapped_column(String,nullable=False)
+    rating:Mapped[float] = mapped_column(Float,nullable=True)
+    ranking:Mapped[int] = mapped_column(Integer,nullable=True)
+    review:Mapped[str] = mapped_column(String,nullable=True)
     img_url:Mapped[str] = mapped_column(String,nullable=False)
 
     def __repr__(self):
@@ -89,6 +106,41 @@ def delete():
     db.session.commit()
     return redirect(url_for("home"))
 
+@app.route("/add",methods=["GET","POST"])
+def add_page():
+    form = Add()
+    if form.validate_on_submit():
+        title = form.movie_title.data
+        parameters = {
+            "query": title,
+            "api_key": api_key,
+        }
+        response = requests.get(url=tmdb_url,params=parameters, headers=headers)
+        data = response.json()["results"]
+        return render_template("select.html",results = data)
+    return render_template("add.html",form = form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_id = request.args.get("movie_id")
+    print(movie_id)
+    movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    print(movie_url)
+    parameters = {
+        "api_key": api_key,
+    }
+    data = requests.get(url = movie_url, params=parameters).json()
+    print(data)
+    new_movie = Movie(
+        title = data["title"],
+        year = data["release_date"].split('-')[0],
+        description = data["overview"],
+        img_url = f"https://image.tmdb.org/t/p/original{data['poster_path']}"
+    )
+    db.session.add(new_movie)
+    db.session.commit()
+    return redirect(url_for("edit",id = new_movie.id))
 
 if __name__ == '__main__':
     app.run(debug=True)
